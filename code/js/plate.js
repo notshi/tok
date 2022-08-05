36b3190d73c142c63daf6f7f82af8c2daf47d60f
kriss <kriss@xixs.com>
2022-08-05 01:13:56 +0100

    plate test

--- code/js/plate.js
+++ code/js/plate.js
@@ -2,8 +2,15 @@
 var plate=exports
 
 
+let plated = require( "plated" ).create({},{pfs:{}})
 
-let plated = require( "plated" )
+plate.chunks={}
+
+plated.chunks.fill_chunks( require('fs').readFileSync(__dirname + '/../../plated/source/^.html', 'utf8'), plate.chunks )
+plated.chunks.format_chunks( plate.chunks )
+
+
+plate.replace=function(s){ return plated.chunks.replace(s,plate.chunks) }
 
 
 plate.setup=async function()
